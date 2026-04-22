57cca82ea62a753dfe582a49554ce609b070d83f
ireneperez9 <irene.perez-lopez@schanzer-racing.de>
2026-04-22 19:44:58 +0200

    Create leftdistance_sensor.cpp

---
+++ src/car_safety_pkg/src/leftdistance_sensor.cpp
@@ -0,0 +1,38 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include <cstdlib>
+#include <ctime>
+
+class leftDistanceSensor : public rclcpp::Node
+{
+public:
+    leftDistanceSensor() : Node("left_distance_sensor")
+    {
+        publisher_ = create_publisher<std_msgs::msg::Float32>("left_distance", 10);
+        timer_ = create_wall_timer(
+            std::chrono::milliseconds(100),
+            std::bind(&leftDistanceSensor::send, this)
+        );
+    }
+
+
+private:
+    void send()
+    {
+        std_msgs::msg::Float32 msg;
+        msg.data = rand() % 200;  // distance between 0 and 199 cm, do not know if is realistic maybe should do it smaller range
+        publisher_->publish(msg);
+    }
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    srand(time(NULL));
+    rclcpp::spin(std::make_shared<leftDistanceSensor>());
+    rclcpp::shutdown();
+}
